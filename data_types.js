d66acfdec40a978887351a1fd2b42e97642a9424
Barsu5489 <ebarsulai@gmail.com>
2020-01-29 12:15:00 +0300

    examples data types in JS

---
+++ data_types.js
@@ -0,0 +1,21 @@
+//scope bindings:var, let, const
+console.log('understanding var')
+var name; //variable declaration
+name = 'chris otta';// variable assignment of type 'string'
+console.log(name);
+name = 'fleria kate';
+console.log(name);
+
+console.log('understanding let')
+let fruit;
+fruit = 'mango';
+console.log(fruit);
+fruit = 'pnnaple';
+console.log(fruit);
+//let fruit = 'pawpaw';
+console.log(fruit);
+//const in action
+const PI = 3.142;
+console.log(PI);
+//PI = 12.32;
+console.log(PI);
